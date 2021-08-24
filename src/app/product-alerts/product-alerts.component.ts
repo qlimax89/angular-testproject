b4700b6492bd0d7ad5c8ad0ef55d5150288a4d13
qlimax89 <kevin_25jan@hotmail.com>
2021-08-24 23:48:57 +0200

    first step with notify on click

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -9,6 +8,8 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
